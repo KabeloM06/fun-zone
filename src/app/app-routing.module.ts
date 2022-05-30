8b19c34bd012cde5ab00fab56c80cafd662165b9
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-05-30 11:14:30 +0200

    set up series routing. error present

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { SeriesComponent } from './components/series/series.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { SeriesDetailsComponent } from './components/series-details/series-details.component';
 
 const routes: Routes = [
   {
@@ -15,9 +16,14 @@ const routes: Routes = [
     component: MoviesComponent
   },
   {
+    path: 'series/:id',
+    component: SeriesDetailsComponent
+  },
+  {
     path: 'details/:id',
     component: MovieDetailsComponent
   },
+  
   {
     path: 'series',
     component: SeriesComponent
